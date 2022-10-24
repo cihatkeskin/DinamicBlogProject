cada674abf06e14aaf609a32338dc07939d0b3f3
Cihat Keskin <chtkskns37@gmail.com>
2022-10-24 20:28:35 +0300

    Register Page Edited.

--- BusinessLayer/Concrete/BlogManager.cs
+++ BusinessLayer/Concrete/BlogManager.cs
@@ -51,5 +51,10 @@ namespace BusinessLayer.Concrete
         {
             return _blogDAL.GetListAll();
         }
+
+        public List<Blog> GetBlogListByWriter(int id)
+        {
+            return _blogDAL.GetListAll(x => x.WriterID == id);
+        }
     }
 }
